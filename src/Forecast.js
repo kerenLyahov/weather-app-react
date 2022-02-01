2e9059fa990cc726a97a559c7098d3c55aae9d86
kerenLyahov <kerenlyahov@gmail.com>
2022-02-01 16:25:34 +0200

    update forecast days
    updated forecast:
    styling,
    day display to actual days

--- src/Forecast.js
+++ src/Forecast.js
@@ -2,14 +2,21 @@ import React from "react";
 import "./Forecast.css";
 
 export default function Forcast() {
+  let date = new Date();
+  let dayName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
   let days = [0, 1, 2, 3, 4];
 
   return (
     <div className="body">
-      {days.map((day, i) => {
+      {days.map((i) => {
         return (
-          <span className="forcast" Day={day} key={i}>
-            <span> Day {i}</span>
+          <span className="forcast" key={i}>
+            <span>
+              {date.getDay() + i + 1 < 7
+                ? dayName[date.getDay() + i + 1]
+                : dayName[date.getDay() + i - 6]}
+            </span>
             <div>☀</div>
             <div>16°/27°</div>
           </span>
